43e9e633993192a8619459546863b24ecd62a2a7
Amir Aijaz <77732195+amirejaz@users.noreply.github.com>
2024-01-11 10:35:35 +0500

    Add files via upload

---
+++ totalCarCounter.py
@@ -0,0 +1,81 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+from sort import *
+
+# Initialize the KalmanBoxTracker
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+limits = [70, 310, 543, 310]
+totalCounts = []
+
+cap = cv2.VideoCapture("Images/cars_resized.mp4")
+cap.set(3, 640)
+cap.set(4, 420)
+
+model = YOLO("../Yolo Weights/yolov8l.pt")
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush" ]
+
+mask = cv2.imread("Images/mask.png")
+mask = cv2.resize(mask, (640, 420))
+
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (640, 420))
+    # imgRegion = cv2.bitwise_and(img, mask)
+    # imgGraphics = cv2.imread("Images/carGraphics.png", cv2.IMREAD_UNCHANGED)
+    # # imgRegion = cv2.bitwise_and(img, imgGraphics)
+    # # imgGraphics = cv2.resize(imgGraphics, (img.shape[1], img.shape[0]))
+    # img = cvzone.overlayPNG(img, imgGraphics, (0, 0))
+    
+    results = model(img, stream=True)
+
+    detections = np.empty((0, 5))
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            cls = int(box.cls[0])
+                           
+            currentClass = classNames[cls]
+            if currentClass in ["car", "motorbike", "truck", "bus"] or conf > 0.3:
+                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1,
+                #                    offset=3)
+                # cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
+    
+    for result in resultsTracker:
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        w, h = x2 - x1, y2 - y1
+        cvzone.cornerRect(img, (x1, y1, w, h), l=4, rt=2, colorR=(255, 0, 0))
+        cvzone.putTextRect(img, f'{int(id)}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1,
+                                   offset=10)
+
+        cx, cy = x1+w//2, y1+h//2
+        cv2.circle(img, (cx, cy), 5,(255, 0, 255), cv2.FILLED)
+        
+        if limits[0] < cx < limits[2] and limits[1]-15 < cy < limits[3]+15:
+            if totalCounts.count(id) == 0:
+                totalCounts.append(id)
+                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 4)
+                
+                
+    cvzone.putTextRect(img, f'Total Counts: {len(totalCounts)}', (50, 50), thickness=2, offset=3, colorR=(255, 0, 0))
+            
+
+    cv2.imshow("Image", img)
+    # cv2.imshow("Image Region", imgRegion)
+
+    if cv2.waitKey(1) & 0xFF == 113:
+        break
+
+cv2.destroyAllWindows()
